a8697e67c5cae625478826cb65a74b70ffa5a9be
Shaurya0802 <62699422+Shaurya0802@users.noreply.github.com>
2020-05-14 16:57:48 +0530

    Update SlingShot.js

--- SlingShot.js
+++ SlingShot.js
@@ -23,7 +23,7 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            push():
+            push();
             strokeWeight(4);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
             pop();
